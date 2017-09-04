82681490e786b12cf48f6367dedb7be0c2bbc796
xregy <xregy@hotmail.com>
2017-09-05 00:10:56 +0900

    2017-09-04

--- draw_two.vert
+++
@@ -1,6 +0,0 @@
-attribute float x;
-attribute float y;
-void main()
-{
-	gl_Position = vec4(x,y,0,1);
-}
---
+++ draw_with_vao.js
@@ -0,0 +1,96 @@
+function main()
+{
+	var canvas = document.getElementById('webgl');
+	var gl = getWebGLContext(canvas);
+    if (!gl)  
+    {
+        console.log('Failed to get the rendering context for WebGL'); 
+        return;
+    }
+
+//https://developer.mozilla.org/en-US/docs/Web/API/OES_vertex_array_object
+    var ext = null;
+    ext = gl.getExtension("OES_vertex_array_object");
+
+    if(!ext)
+    {
+        console.log('OES_vertex_array_object not supported.');
+        return;
+    }
+
+	var positions_x = [
+        new Float32Array([
+			-0.90, // Triangle 1
+			0.85,
+			-0.90,
+            ]),
+        new Float32Array([
+			0.90, // Triangle 2
+			0.90,
+			-0.85,
+            ])
+        ];
+
+	var positions_y = [
+        new Float32Array([
+			-0.90, // Triangle 1
+			-0.90,
+			0.85,
+            ]),
+        new Float32Array([
+			-0.85, // Triangle 2
+			0.90,
+			0.90
+            ])
+        ];
+
+	var id_vert = ['shader-vert', "shader-vert"];
+	var id_frag = ["shader-frag-red", "shader-frag-blue"];
+    var h_prog = Array([null, null]);
+	var	vao = Array([null, null]);
+	var	vbo_x = Array([null, null]);
+	var	vbo_y = Array([null, null]);
+
+	for(var i=0 ; i<2 ; i++)
+	{
+		var src_vert = document.getElementById(id_vert[i]).text;
+		var src_frag = document.getElementById(id_frag[i]).text;
+
+	    if (!initShaders(gl, src_vert, src_frag))
+		{
+        	console.log('Failed to intialize shaders.');
+        	return;
+		}
+    	h_prog[i] = gl.program;
+
+	    vao[i] = ext.createVertexArrayOES();
+	    ext.bindVertexArrayOES(vao[i]); 
+
+	    vbo_x[i] = gl.createBuffer();
+	    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_x[i]);
+	    gl.bufferData(gl.ARRAY_BUFFER, positions_x[i], gl.STATIC_DRAW);
+	    var loc_x = gl.getAttribLocation(h_prog[i], 'x');
+	    gl.vertexAttribPointer(loc_x, 1, gl.FLOAT, false, 0, 0);
+	    gl.enableVertexAttribArray(loc_x);
+
+	    vbo_y[i] = gl.createBuffer();
+	    gl.bindBuffer(gl.ARRAY_BUFFER, vbo_y[i]);
+	    gl.bufferData(gl.ARRAY_BUFFER, positions_y[i], gl.STATIC_DRAW);
+	    var loc_y = gl.getAttribLocation(h_prog[i], 'y');
+	    gl.vertexAttribPointer(loc_y, 1, gl.FLOAT, false, 0, 0);
+	    gl.enableVertexAttribArray(loc_y);
+    }
+	ext.bindVertexArrayOES(null); 
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+//-----------------------------------------------------
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+	for(var i=0 ; i<2 ; i++)
+	{
+	    gl.useProgram(h_prog[i]);
+		ext.bindVertexArrayOES(vao[i]); 
+		gl.drawArrays(gl.TRIANGLES, 0, 3);
+	}
+}
