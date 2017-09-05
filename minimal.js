307e5c2c4790ad69947a02eca175911926f1a0ba
xregy <xregy@hotmail.com>
2017-09-05 13:37:57 +0900

    .

--- minimal.js
+++ minimal.js
@@ -1,30 +1,21 @@
 function main()
 {
-  var canvas = document.getElementById('webgl');
-  var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
-  var gl = null;
-  for (var i = 0; i < names.length; ++i)
-  {
-    try
+    var canvas = document.getElementById('webgl');
+    var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+    var gl = null;
+    for (var i = 0; i < names.length; ++i)
     {
-      gl = canvas.getContext(names[i], []);
+          gl = canvas.getContext(names[i], []);
+          if (gl) break;
     }
-    catch(e)
+    
+    if (!gl)
     {
+        console.log('Failed to get the rendering context for WebGL'); return;
     }
-    if (gl)
+    else
     {
-      break;
+        gl.clearColor(1.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
     }
-  }
-
-  if (!gl)
-  {
-    console.log('Failed to get the rendering context for WebGL'); return;
-  }
-  else
-  {
-    gl.clearColor(1.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-  }
 }
