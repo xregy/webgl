9ae809cc05d4dda03edbff0bf2a5e675e0300a25
minho <xregy@hotmail.com>
2018-11-29 15:26:22 +0900

    .

--- src/class_texture.js
+++ src/class_texture.js
@@ -2,11 +2,11 @@
 
 class Texture
 {
-	constructor(gl, image)
+	constructor(gl, image, flipY = true)
 	{
 		this.texid = gl.createTexture(); 
 		gl.bindTexture(gl.TEXTURE_2D, this.texid);
-		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);// Flip the image's y-axis
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flipY);// Flip the image's y-axis
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 	}
