268fe8513224c8015fef8f72e48d5f63589703f9
minho <xregy@hotmail.com>
2018-12-12 10:05:46 +0900

    .

--- src/class_texture.js
+++ src/class_texture.js
@@ -11,3 +11,21 @@ class Texture
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 	}
 }
+
+class TextureCubemap
+{
+	constructor(gl, images)
+	{
+		this.texid = gl.createTexture(); 
+		gl.bindTexture(gl.TEXTURE_CUBE_MAP, this.texid);
+		gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+		gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.posx);
+		gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.negx);
+		gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.posy);
+		gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.negy);
+		gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.posz);
+		gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.negz);
+	}
+}
--- src/tex-cubemap.js
+++ src/tex-cubemap.js
@@ -31,23 +31,24 @@ function main()
 	let V = new Matrix4();
 	V.translate(0, 0, -10);
 
-	let tex_cubemap = gl.createTexture();
+	let tex_cubemap;
 	const TEX_UNIT = 3;
 	gl.activeTexture(gl.TEXTURE0 + TEX_UNIT);
-	gl.bindTexture(gl.TEXTURE_CUBE_MAP, tex_cubemap);
-	gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+	gl.useProgram(shader_room.h_prog);
+	gl.uniform1i(gl.getUniformLocation(shader_room.h_prog, "sampler_cubemap"), TEX_UNIT);
+	gl.useProgram(shader_cubemap.h_prog);
+	gl.uniform1i(gl.getUniformLocation(shader_cubemap.h_prog, "sampler_cubemap"), TEX_UNIT);
 
 	let img_cubemap =
 	{
-		posx:{image:new Image(), target:gl.TEXTURE_CUBE_MAP_POSITIVE_X},
-		negx:{image:new Image(), target:gl.TEXTURE_CUBE_MAP_NEGATIVE_X},
-		posy:{image:new Image(), target:gl.TEXTURE_CUBE_MAP_POSITIVE_Y},
-		negy:{image:new Image(), target:gl.TEXTURE_CUBE_MAP_NEGATIVE_Y},
-		posz:{image:new Image(), target:gl.TEXTURE_CUBE_MAP_POSITIVE_Z},
-		negz:{image:new Image(), target:gl.TEXTURE_CUBE_MAP_NEGATIVE_Z},
+		posx:new Image(),
+		negx:new Image(),
+		posy:new Image(),
+		negy:new Image(),
+		posz:new Image(),
+		negz:new Image(),
 	};
-	let faces = ["posx", "negx", "posy", "negy", "posz", "negz"];
 
 	const	TOTAL_IMAGES = 6;
 	let	images_loaded = 0;
@@ -55,6 +56,7 @@ function main()
 	let tick_init = function() {
 		if(images_loaded == TOTAL_IMAGES)
 		{
+			tex_cubemap = new TextureCubemap(gl, img_cubemap);
 			requestAnimationFrame(tick, canvas); // Request that the browser calls tick
 		}
 		else
@@ -62,6 +64,7 @@ function main()
 			requestAnimationFrame(tick_init, canvas); // Request that the browser calls tick
 		}
 	};
+
 
 	let t_last = Date.now();
 	const ANGLE_STEP = 45;
@@ -74,30 +77,15 @@ function main()
 		monkey.M.rotate(( (ANGLE_STEP * elapsed) / 1000.0) % 360.0, 0, 1, 0);
 
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-		gl.useProgram(shader_room.h_prog);
-		gl.uniform1i(gl.getUniformLocation(shader_room.h_prog, "sampler_cubemap"), TEX_UNIT);
 		room.render(gl, shader_room, null, null, V, P);
-
-		gl.useProgram(shader_cubemap.h_prog);
-		gl.uniform1i(gl.getUniformLocation(shader_cubemap.h_prog, "sampler_cubemap"), TEX_UNIT);
 		monkey.render(gl, shader_cubemap, null, null, V, P);
-
 		requestAnimationFrame(tick, canvas); // Request that the browser calls tick
 	};
 
-	for(let face of faces)
+	for(let face of ["posx", "negx", "posy", "negy", "posz", "negz"])
 	{
-		img_cubemap[face].image.onload = function()
-		{
-			gl.texImage2D(img_cubemap[face].target, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img_cubemap[face].image);
-			images_loaded++;
-		};
-	}
-
-	for(let face of faces)
-	{
-		img_cubemap[face].image.src = "../resources/SwedishRoyalCastle/" + face + ".jpg";
+		img_cubemap[face].onload = function() { images_loaded++; };
+		img_cubemap[face].src = "../resources/SwedishRoyalCastle/" + face + ".jpg";
 	}
 	
 	tick_init();
@@ -105,52 +93,16 @@ function main()
 }
 
 
-function render_object(gl, shader, object)
-{
-	gl.useProgram(shader.h_prog);
-	shader.set_uniforms(gl);
-
-	for(let attrib_name in shader.attribs)
-	{
-		let	attrib = object.attribs[attrib_name];
-		gl.bindBuffer(gl.ARRAY_BUFFER, attrib.buffer);
-		gl.vertexAttribPointer(shader.attribs[attrib_name], attrib.size, attrib.type, attrib.normalized, attrib.stride, attrib.offset);
-		gl.bindBuffer(gl.ARRAY_BUFFER, null);
-		gl.enableVertexAttribArray(shader.attribs[attrib_name]);
-	}
-	if(object.drawcall == "drawArrays")
-	{
-		gl.drawArrays(object.type, 0, object.n);
-	}
-	else if(object.drawcall == "drawElements")
-	{
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, object.buf_index);
-		gl.drawElements(object.type, object.n, object.type_index, 0);
-	}
-
-	for(let attrib_name in object.attribs)
-	{
-		gl.disableVertexAttribArray(shader.attribs[attrib_name]);
-	}
-
-	gl.useProgram(null);
-}
-
-
-function init_tex_cubemap_face(gl, tex, img)
-{
-}
-
 function create_mesh_room_cubemap(gl)
 {
-    // Create a cube
-    //    v6----- v5
-    //   /|      /|
-    //  v1------v0|
-    //  | |     | |
-    //  | |v7---|-|v4
-    //  |/      |/
-    //  v2------v3
+	// Create a cube
+	//    v6----- v5
+	//   /|      /|
+	//  v1------v0|
+	//  | |     | |
+	//  | |v7---|-|v4
+	//  |/      |/
+	//  v2------v3
 	var verts = new Float32Array([
 	// Note the triangles are facing inside.
 		 1, 1, 1,	// v0
