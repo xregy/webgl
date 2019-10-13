7c65d268c99111f252b0aa48517460c60a089b7e
minho <xregy@hotmail.com>
2019-10-13 21:38:14 +0900

    .

--- src/instanced.js
+++ src/instanced.js
@@ -1,25 +1,32 @@
 "use strict";
 const loc_aPosition = 3;
+const loc_aColor = 8;
+const loc_aScale = 7;
 const VSHADER_SOURCE =
 `#version 300 es
 #define M_PI 3.1415926535897932384626433832795
 layout(location=${loc_aPosition}) in vec4 aPosition;
+layout(location=${loc_aColor}) in vec4 aColor;
+layout(location=${loc_aScale}) in float aScale;
 uniform float uN;
+out vec4 vColor;
 void main() {
     float angle = 2.0*M_PI*float(gl_InstanceID)/uN;
     float c = cos(angle);
     float s = sin(angle);
     mat4 R = mat4(c, s, 0, 0, -s, c, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
-    gl_Position = R*aPosition;
+    gl_Position = R*vec4(aScale*aPosition.xyz, 1);
+    vColor = aColor;
 }`;
 
 // Fragment shader program
 const FSHADER_SOURCE =
 `#version 300 es
 precision mediump float;
+in vec4 vColor;
 out vec4 fColor;
 void main() {
-    fColor = vec4(1.0, 0.0, 0.0, 1.0);
+    fColor = vColor;
 }`;
 
 function main() {
@@ -37,12 +44,12 @@ function main() {
     let loc_uN;
     loc_uN = gl.getUniformLocation(gl.program, 'uN');
 
-    let {vao, n} = initVertexBuffers(gl);
+    let {vao, n} = initVAO(gl);
 
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
     
-    const N = 10;
+    const N = 6;
     gl.uniform1f(loc_uN, N);
 
     gl.bindVertexArray(vao);
@@ -50,9 +57,18 @@ function main() {
     gl.bindVertexArray(null);
 }
 
-function initVertexBuffers(gl) {
+function initVAO(gl) {
     const vertices = new Float32Array([
-        0, 0, -0.1, -0.5, 0.1, -0.5
+        0, 0, -0.1, -0.5, 0.1, -0.5,
+        1.5,
+        1.0,
+        0.5,
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1,
+        0, 1, 1,
+        1, 0, 1,
+        1, 1, 0
     ]);
     const n = 3; // The number of vertices
     
@@ -62,9 +78,21 @@ function initVertexBuffers(gl) {
     let vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    const FSIZE = vertices.BYTES_PER_ELEMENT;
     
     gl.vertexAttribPointer(loc_aPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(loc_aPosition);
+    gl.vertexAttribDivisor(loc_aPosition, 0);
+
+
+    gl.vertexAttribPointer(loc_aScale, 1, gl.FLOAT, false, 0, FSIZE*(6));
+    gl.enableVertexAttribArray(loc_aScale);
+    gl.vertexAttribDivisor(loc_aScale, 2);
+
+    gl.vertexAttribPointer(loc_aColor, 3, gl.FLOAT, false, 0, FSIZE*(6+3));
+    gl.enableVertexAttribArray(loc_aColor);
+    gl.vertexAttribDivisor(loc_aColor, 1);
     
     gl.bindVertexArray(null);
     
