f514568cdd018e9fe9023e49e67048bedf757c6e
minho <xregy@hotmail.com>
2019-10-15 15:31:25 +0900

    .

---
+++ examples-ported/ch05/TexturedQuad-sampler.js
@@ -0,0 +1,145 @@
+// TexturedQuad.js (c) 2012 matsuda and kanda
+// Vertex shader program
+"use strict";
+const loc_aPosition = 3;
+const loc_aTexCoord = 7;
+const tex_unit = 3;
+const VSHADER_SOURCE =
+`#version 300 es
+layout(location=${loc_aPosition}) in vec4 aPosition;
+layout(location=${loc_aTexCoord}) in vec2 aTexCoord;
+out vec2 vTexCoord;
+void main() {
+  gl_Position = aPosition;
+  vTexCoord = aTexCoord;
+}`;
+
+// Fragment shader program
+const FSHADER_SOURCE =
+`#version 300 es
+precision mediump float;
+uniform sampler2D uSampler;
+in vec2 vTexCoord;
+out vec4 fColor;
+void main() {
+  fColor = texture(uSampler, vTexCoord);
+}`;
+
+function main() {
+  // Retrieve <canvas> element
+  const canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  const gl = canvas.getContext('webgl2');
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Set the vertex information
+  let {vao,n} = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Set texture
+  if (!initTextures(gl, vao, n)) {
+    console.log('Failed to intialize the texture.');
+    return;
+  }
+}
+
+function initVertexBuffers(gl) {
+  const verticesTexCoords = new Float32Array([
+    // Vertex coordinates, texture coordinate
+    -0.5,  0.5,   0.0, 1.0,
+    -0.5, -0.5,   0.0, 0.0,
+     0.5,  0.5,   1.0, 1.0,
+     0.5, -0.5,   1.0, 0.0,
+  ]);
+  const n = 4; // The number of vertices
+
+  let vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  // Create the buffer object
+  let vertexTexCoordBuffer = gl.createBuffer();
+  if (!vertexTexCoordBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+
+  const FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+
+  gl.vertexAttribPointer(loc_aPosition, 2, gl.FLOAT, false, FSIZE * 4, 0);
+  gl.enableVertexAttribArray(loc_aPosition);  // Enable the assignment of the buffer object
+
+  gl.vertexAttribPointer(loc_aTexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+  gl.enableVertexAttribArray(loc_aTexCoord);  // Enable the assignment of the buffer object
+
+  gl.bindVertexArray(null);
+
+  return {vao, n};
+}
+
+function initTextures(gl, vao, n) {
+  let texture = gl.createTexture();   // Create a texture object
+  if (!texture) {
+    console.log('Failed to create the texture object');
+    return false;
+  }
+
+  // Get the storage location of uSampler
+  let loc_uSampler = gl.getUniformLocation(gl.program, 'uSampler');
+  if (!loc_uSampler) {
+    console.log('Failed to get the storage location of uSampler');
+    return false;
+  }
+  let image = new Image();  // Create the image object
+  if (!image) {
+    console.log('Failed to create the image object');
+    return false;
+  }
+  // Register the event handler to be called on loading an image
+  image.onload = function(){ loadTexture(gl, vao, n, texture, loc_uSampler, image); };
+
+  // Tell the browser to load an image
+  image.src = '../resources/sky.jpg';
+
+
+  return true;
+}
+
+function loadTexture(gl, vao, n, texture, loc_uSampler, image) {
+
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+
+  gl.activeTexture(gl.TEXTURE0 + tex_unit);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+  let sampler = gl.createSampler();
+
+  gl.samplerParameteri(sampler, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.bindSampler(tex_unit, sampler);
+  gl.uniform1i(loc_uSampler, tex_unit);
+  
+  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+  gl.bindVertexArray(vao);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+  gl.bindVertexArray(null);
+}
